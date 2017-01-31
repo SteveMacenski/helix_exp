47af0201b60f9a0122933f6d7b4d3ac488945297
stevemacenski <stevenmacenski@gmail.com>
2017-01-31 14:37:26 -0600

    post-processing updates and example photos

--- post_processing/im_seg.cpp
+++ post_processing/im_seg.cpp
@@ -8,45 +8,75 @@
 #include <string>
 #include <fstream>
 #include <ctime>
+#include <algorithm>
+#include <unistd.h>
 
 using namespace cv;
 
+Mat flip_90(Mat& src){
 
-//TODO rotate image 90 degrees, then myROI for new setup w. horiz. cam. data in ID/T
+
+  transpose(src, src);   
+  flip(src, src, 0);
+
+  Rect myROI(0, 500, 1080, 1100);
+  src = src(myROI);
+  
+  return src;
+
+}
+
+Mat flip_270(Mat& src){
+
+
+  transpose(src, src);   
+  flip(src, src, 0);
+  
+  return src;
+
+}
 
 Mat Threshold( Mat& src, char* window_name )
 {
-  int threshold_value = 54;
+  int threshold_value = 70;
   int const max_BINARY_value = 255;
   int threshold_type = 1;
 
   threshold( src, src, threshold_value, max_BINARY_value,threshold_type );
   
-  Rect myROI(250, 50, 1050, 1000);
+  Rect myROI(0, 0, 1080-80, 1000);
   src = src(myROI);
   
-  imshow( window_name, src );
+                //imshow( window_name, src );
   
   return src;
 }
 
-Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
+bool point_comparator(const cv::Point &a, const cv::Point &b) {
+    return (a.x < b.x);
+}
+
+void Find_Rod( Mat& src, char* window_name2, char* file_name )
 {
+
   std::vector<int> equipment_forward;
   std::vector<int> equipment_backward;
   equipment_forward.resize(src.cols);
   equipment_backward.resize(src.cols);
   std::vector<int> rod;
-  double percentCoverage = .02;
+  double percentCoverage = .012; // was 0.02
   int is_equip;
-  int smoothing = 70;
+  int smoothing = 20;
   int starting, ending;
   Mat source;
   source = src.clone();
   bool first = false;
   int hor_starting, hor_ending;
   int hor_height, height;
-  
+  Mat hor_src;
+  Mat vert_src;
+  Mat currentROI;
+
   //for all pixels sweeping vertically forwards
   for (int col = 0; col < src.cols; col++){
     equipment_forward[col] = 0;
@@ -55,7 +85,7 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
          equipment_forward[col]++; 
       }
     }
-
+    
     if (equipment_forward[col] > percentCoverage*src.rows){
       // this is potentially robot
       
@@ -67,7 +97,7 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
             is_equip++;
           }
         } 
-        if (is_equip <= smoothing*0.5){
+        if (is_equip <= smoothing*0.9){
           //actually rod
           continue;
         }  
@@ -75,12 +105,11 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
       
       //to visualize what it marked, last after smoothing
       for (int row = 0; row <= src.rows; row++){
-        src.at<uchar>(col, row) = 0;
+        //src.at<uchar>(col, row) = 1;
       }
     }
   }
-  
-  
+
   //for all pixels sweeping vertically backwards
   for (int col = src.cols; col > 0; col--){
     equipment_backward[src.cols-col] = 0;
@@ -105,69 +134,69 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
           }
         } 
         
-        if (is_equip <= smoothing*0.5){
+        if (is_equip <= smoothing*0.9){
           //actually rod
           continue;
-        }  
+        } 
       }
 
       //to visualize what it marked, last after smoothing
       for (int row = 0; row <= src.rows; row++){
-        src.at<uchar>(col, row) = 0;
+        //src.at<uchar>(col, row) = 1;
       }
     }
   } 
 
-  //imwrite("horizontal_band_im.bmp", src);
-
-
   // remove black pixels so that its just remaining ones
+
   
   for (int col = 0; col < src.cols; col++){
     if (equipment_forward[col] < percentCoverage*src.rows && col > 50){
       starting = col;
+          //TODO make starting/ending false positive proof
       break;
-      }
     }
-    
+  }
+
   for (int col = 0; col < src.cols; col++){
-    if (equipment_backward[col] < src.rows && col > 50){
+    if ((equipment_backward[col] < percentCoverage*src.rows) && (col > 50)){
       ending = src.cols - col;
       break;
-      }
     }
-    
+  }
+
   height = ending-starting-smoothing-2; //2 to get rid of pot. bars
   Rect myROI(0, starting+2, src.cols, height);
-  src = src(myROI);
-  
-  //imwrite("crop_horizontal_im.bmp", src);
-  
+
+  hor_src = src(myROI);
+
+                //namedWindow( "horizontal_band_im.bmp", 1000 );
+                //imshow("horizontal_band_im.bmp", hor_src); 
+                //imwrite("crop_horizontal_im.bmp", hor_src);
+
   // for remaining pixels, sweep horizontally
-  rod.resize(src.cols);
+  rod.resize(hor_src.cols);
   
-  for (int row = 0; row < src.cols; row++){
+  for (int row = 0; row < hor_src.cols; row++){
     rod[row] = 0; 
-    for (int col = 0; col < src.rows; col++){
-      if (src.at<uchar>(col,row) == 0){
+    for (int col = 0; col < hor_src.rows; col++){
+      if (hor_src.at<uchar>(col,row) == 0){
          rod[row]++; 
       }
     }
 
-    if (rod[row] < percentCoverage*src.rows/4){
+    if (rod[row] < percentCoverage*hor_src.rows/4){
       //potentially rod
-      for (int col = 0; col < src.cols; col++){
-        src.at<uchar>(col, row) = 0;
+      for (int col = 0; col < hor_src.cols; col++){
+        //hor_src.at<uchar>(col, row) = 0; // TODO why removed
       }
     }
 
   }
 
-  //imwrite("vertical_band_im.bmp", src);
-  
   // remove black pixels so that its just remaining ones
-  for (int row = 0; row < src.cols; row++){
-    if (rod[row] > percentCoverage*src.rows/4 && row > 5){
+  for (int row = 0; row < hor_src.cols; row++){
+    if (rod[row] > percentCoverage*hor_src.rows/4 && row > 50){
       if (first == false){
         hor_starting = row;
         first = true;
@@ -176,11 +205,16 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
       }
     }
     
-  hor_height = hor_ending-hor_starting-7; //tuned, 10 works too
-  Rect ROI(hor_starting, 0, hor_height, src.rows);
-  src = src(ROI);
+  hor_height = hor_ending-hor_starting-0; //7-10 works
+  Rect ROI(hor_starting, 0, hor_height, hor_src.rows);
 
-  //imwrite("vertical_crop_im.bmp", src);
+  vert_src = hor_src(ROI);
+
+                //namedWindow( "vertical_band_im.bmp", 1000 );
+                //imshow("vertical_band_im.bmp", vert_src); 
+                //imwrite("Avertical_crop_im.bmp", vert_src);
+  
+  //////////////////////////////////////////////////////////////
   
   // subsample points on the curve evenly
   
@@ -189,18 +223,17 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
   std::vector<cv::Point> rod_points_subsampled_picF;
   
   int sub_ROI = 50;
-  int x = src.rows/sub_ROI;
-  Mat currentROI;
-  
-  for (int row=1; row < src.rows/x; row++){
-    
+  int x = vert_src.rows/sub_ROI;
+
+  //TODO start issues 7
+  for (int row=1; row < vert_src.rows/x-1; row++){
     //sets ROI and slices to it
-    Rect ROIsize(0, row*x, src.cols, 4);
-    currentROI = src(ROIsize);
-      
+    Rect ROIsize(0, row*x, vert_src.cols, 4);
+    currentROI = vert_src(ROIsize);
+
     //clears the point count
     rod_points.clear();
-      
+
     //finds points in ROI
     for (int colI = 0; colI < currentROI.cols; colI++){
       for (int rowI = 0; rowI < currentROI.rows; rowI++){
@@ -209,34 +242,199 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
         }
       }
     }
+
+
+    ////////////////////////////////////////////////////////////////
+    
+    
     if (rod_points.size()>10){
-    //with points find center pixel
-      int x_pos = 0;
-      int y_pos = 0;
-      for(int j=0;j<rod_points.size();j++){
-        x_pos += rod_points[j].x;
-        y_pos += rod_points[j].y;
-      }
-      x_pos /= rod_points.size();
-      y_pos /= rod_points.size();
+    //with points find center pixel, 10 for removing small noise bits
+    
+      // sorted list of points
+      // std::vector<cv::Point> sorted_rod_points(rod_points);
+      // std::sort(sorted_rod_points.begin(), sorted_rod_points.end(), 
+                                                 //point_comparator);
+           
+      // if distance between highest and lowest is high      
+      /* if (abs(sorted_rod_points.front().x-
+                   sorted_rod_points.back().x) > 30){
         
+        // if there may be a 3rd cluster
+        if (abs(sorted_rod_points.front().x-sorted_rod_points[sorted_rod_points.size()/2].x) > 30 && 
+            abs(sorted_rod_points.back().x-sorted_rod_points[sorted_rod_points.size()/2].x) > 30){
+          std::cout << "THREE CLUSTERS" << std::endl;
+          //3 clusters
+          int max_distance = 0;
+          int secondary_distance = 0;
+          int split_index;
+          int secondary_index;
+          for (int g=0;g<sorted_rod_points.size()-1; g++){
+          //check max distance
+            if (abs(sorted_rod_points[g].x-sorted_rod_points[g+1].x) > max_distance){
+              max_distance = abs(sorted_rod_points[g].x-sorted_rod_points[g+1].x);
+              split_index = g;
+            }
+            else{
+              if (abs(sorted_rod_points[g].x-sorted_rod_points[g+1].x) > secondary_distance){
+                secondary_distance =abs(sorted_rod_points[g].x-sorted_rod_points[g+1].x);
+                secondary_index=g;
+              }
+            }
+          }
+          //new vector for each cluster
+          if (secondary_index < split_index){
+            int temp = split_index;
+            split_index = secondary_index;
+            secondary_index = split_index;
+          }
+
+          std::vector<cv::Point> c1(sorted_rod_points);
+          std::vector<cv::Point> c2(sorted_rod_points);
+          std::vector<cv::Point> c3(sorted_rod_points);
+          //end
+          c1.erase(c1.begin(), c1.begin()+split_index+secondary_index);
+          //beginning
+          c2.erase(c2.begin()+split_index, c2.end());
+          //middle
+          c3.erase(c3.begin(), c3.begin()+split_index);
+          c3.erase(c3.begin()+secondary_index-split_index, c3.end());
+
+          int x_pos = 0;
+          int y_pos = 0;
+          for (int k=0;k<c1.size();k++){
+            x_pos += c1[k].x;
+            y_pos += c1[k].y;
+          }
+          x_pos /= c1.size();
+          y_pos /= c1.size();
+
+          rod_points_subsampled_rodF.push_back(Point(x_pos,y_pos+row*x));
+          rod_points_subsampled_picF.push_back(Point(x_pos+hor_starting, 
+                                                 y_pos+row*x+starting));
+          circle(src,Point(x_pos, y_pos+row*x),x,1);
+          circle(source,Point(x_pos +hor_starting, 
+                                  y_pos+row*x+starting),x,1);
+
+          x_pos=0;y_pos=0;
+          for (int k=0;k<c2.size();k++){
+            x_pos += c2[k].x;
+            y_pos += c2[k].y;
+          }
+          x_pos /= c2.size();
+          y_pos /= c2.size();
+
+          rod_points_subsampled_rodF.push_back(Point(x_pos, y_pos+row*x));
+          rod_points_subsampled_picF.push_back(Point(x_pos+hor_starting,
+                                                 y_pos+row*x+starting));
+
+          circle(src,Point(x_pos, y_pos+row*x),x,1);
+          circle(source,Point(x_pos+hor_starting, 
+                                  y_pos+row*x+starting),x,1);
+
+          x_pos=0;y_pos=0;
+          for (int k=0;k<c3.size();k++){
+            x_pos += c3[k].x;
+            y_pos += c3[k].y;
+          }
+          x_pos /= c3.size();
+          y_pos /= c3.size();
+
+          rod_points_subsampled_rodF.push_back(Point(x_pos, y_pos+row*x));
+          rod_points_subsampled_picF.push_back(Point(x_pos+hor_starting,
+                                                 y_pos+row*x+starting));
+
+          circle(src,Point(x_pos, y_pos+row*x),x,1);
+          circle(source,Point(x_pos+hor_starting, 
+                        y_pos+row*x+starting),x,1);              
+          }
+      
+      
+      
+        else{ 
+          std::cout << "TWO CLUSTERS" << std::endl;
+          // 2 clusters
+          
+          int max_distance = 0;
+          int split_index;
+          for (int g=0;g<sorted_rod_points.size()-1; g++){
+          //check max distance
+            if (abs(sorted_rod_points[g].x-sorted_rod_points[g+1].x) > max_distance){
+              max_distance = abs(sorted_rod_points[g].x-sorted_rod_points[g+1].x);
+              split_index = g;
+            }
+          }
+          //new vector for each cluster
+          std::vector<cv::Point> c1(sorted_rod_points);
+          std::vector<cv::Point> c2(sorted_rod_points);
+          c1.erase(c1.begin(), c1.begin()+split_index);
+          c2.erase(c2.begin()+split_index, c2.end());
+
+          int x_pos = 0;
+          int y_pos = 0;
+          for (int k=0;k<c1.size();k++){
+            x_pos += c1[k].x;
+            y_pos += c1[k].y;
+          }
+          x_pos /= c1.size();
+          y_pos /= c1.size();
+
+          rod_points_subsampled_rodF.push_back(Point(x_pos,y_pos+row*x));
+          rod_points_subsampled_picF.push_back(Point(x_pos+hor_starting, 
+                                                 y_pos+row*x+starting));
+          circle(src,Point(x_pos, y_pos+row*x),x,1);
+          circle(source,Point(x_pos +hor_starting, 
+                                  y_pos+row*x+starting),x,1);
+
+          x_pos=0;y_pos=0;
+          for (int k=0;k<c2.size();k++){
+            x_pos += c2[k].x;
+            y_pos += c2[k].y;
+          }
+          x_pos /= c2.size();
+          y_pos /= c2.size();
+
+          rod_points_subsampled_rodF.push_back(Point(x_pos, y_pos+row*x));
+          rod_points_subsampled_picF.push_back(Point(x_pos+hor_starting,
+                                                 y_pos+row*x+starting));
+
+          circle(src,Point(x_pos, y_pos+row*x),x,1);
+          circle(source,Point(x_pos+hor_starting, 
+                                  y_pos+row*x+starting),x,1);
+        }
+          
+      }
+          
+      else {*/
+        // 1 cluster
+        int x_pos = 0;
+        int y_pos = 0;
+        for(int j=0;j<rod_points.size();j++){
+          x_pos += rod_points[j].x;
+          y_pos += rod_points[j].y;
+        }
+        x_pos /= rod_points.size();
+        y_pos /= rod_points.size();
+      
       rod_points_subsampled_rodF.push_back(Point(x_pos, y_pos+row*x));
       rod_points_subsampled_picF.push_back(Point(x_pos+hor_starting,
                                              y_pos+row*x+starting));
         
-      circle(src,Point(x_pos, y_pos+row*x),x,1);
+      circle(vert_src,Point(x_pos, y_pos+row*x),x+3,1);
       circle(source,Point(x_pos +hor_starting,
-                                         y_pos+row*x+starting),x,1);
+                                         y_pos+row*x+starting),x+3,1);
+                                         
+      //}
     }
   }
-  
+
+///////////////////////////////////////////////////////////////////
 
   // show the ROI on the original image
   rectangle(source,Point(hor_starting,starting), 
-                            Point(hor_ending-7,ending-72), 1);
-  namedWindow( "ROI", 1000 );
-  imshow( "ROI", source );
-  
+                            Point(hor_ending,height+starting), 1);
+                //namedWindow( "ROI", 1000 );
+                //imshow( "ROI", flip_270(source) );
+                  
   //save pnts+ROI over source threshold for troubleshooting
   std::string file_write = std::string(file_name);
   size_t lastIndex = file_write.find_last_of(".");
@@ -244,7 +442,7 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
   std::string Tpoint = fileName;
   fileName.append("ROIandPnts");
   fileName.append(".jpg");
-  imwrite(fileName, source, (std::vector<int>){CV_IMWRITE_JPEG_QUALITY,0});
+  imwrite(fileName, flip_270(source), (std::vector<int>){CV_IMWRITE_JPEG_QUALITY,0});
   
   //write to file 1 pnt/line, 1 space between rod shapes, w/ date
   
@@ -253,7 +451,7 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
   
   std::fstream myfile;
   myfile.open("rodPoints.txt", std::ios::in | std::ios::out | std::ios::ate);
-  std::cout << Tpoint << std::endl;
+
   myfile << Tpoint;
   myfile << " Date: ";
   myfile << dt;
@@ -264,56 +462,50 @@ Mat Find_Rod( Mat& src, char* window_name2, char* file_name )
   
   myfile.close();
 
-  // show the rod with ROI and points 
-  imshow( window_name2, src );
-  return src;
+                // show the rod with ROI and points 
+                //imshow( window_name2, vert_src );
 }
 
-/*Mat flip_90(Mat& src){
-
-  for(int i = 0; i != 2; ++i){
-    transpose(src, src);   
-    flip(src, src, 0);
-  }
-  
-  Rect myROI(350, 20, 1250, 880);
-  rectangle(src,Point(350,20),Point(1600,900),200);
-  src = src(myROI);
-  
-  imshow( window_name, src );
-  
-  return src;
-
-}
-*/
 
 int main( int argc, char** argv )
 {
   Mat src; Mat src_gray; Mat src_thresh_gray;
   Mat seg_rod;
- 
+
   src = imread(argv[1], 1 );
-  
+
   ///flip sideways to work
-  //src=flip_90(src);
+  src=flip_90(src);
 
   /// Convert the image to Gray
   cvtColor( src, src_gray, CV_BGR2GRAY );
-  //imwrite("gray_scaled_im.bmp", src_gray);
-  
+                //imwrite("gray_scaled_im.bmp", src_gray);
+
   /// Create a window to display results
   char* window_name = "Threshold Robot";
   char* window_name2 = "Segment Out Robot";
-  namedWindow( window_name, 1000 );
-  namedWindow( window_name2, 1000 );
-  
+                //namedWindow( window_name, 1000 );
+                //namedWindow( window_name2, 1000 );
+
   /// threshold the grey scaled image
   src_thresh_gray = Threshold( src_gray, window_name );
-  //imwrite("thresh_im.bmp", src_thresh_gray);
-  
+                //imwrite("thresh_im.bmp", src_thresh_gray);
+
   /// Loop over each vertical, % above N, is robot
-  seg_rod = Find_Rod( src_thresh_gray, window_name2, argv[1] );
-  
+  while (true)
+  {
+    try
+    {
+      Find_Rod( src_thresh_gray, window_name2, argv[1] );
+      break;
+    }
+    catch(std::exception& e)
+    {
+      std::cout << "try again" << std::endl;
+    }
+  }
+  std::cout << "actual finish" << std::endl;
+        
   waitKey(0);
   return(0);
   /// Wait until user finishes program
